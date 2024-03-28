10f8d5dbeb0a16e018416ccfb4128d61fc225529
toyota-m2k <toyota.mitsuki@gmail.com>
2024-03-28 20:36:22 +0900

    feat: UtImmortalViewModel

---
+++ dialog/src/main/java/io/github/toyota32k/dialog/task/UtImmortalViewModel.kt
@@ -0,0 +1,15 @@
+package io.github.toyota32k.dialog.task
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import io.github.toyota32k.dialog.task.IUtImmortalTaskContext
+import io.github.toyota32k.dialog.task.IUtImmortalTaskMutableContextSource
+
+open class  UtImmortalViewModel:ViewModel(), IUtImmortalTaskMutableContextSource {
+    override lateinit var immortalTaskContext: IUtImmortalTaskContext
+}
+
+open class  UtImmortalAndroidViewModel(application: Application):AndroidViewModel(application), IUtImmortalTaskMutableContextSource {
+    override lateinit var immortalTaskContext: IUtImmortalTaskContext
+}
