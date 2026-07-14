a38b987e7307ce1ddcd516e0b81f8796c137d5d4
toyota-m2k <toyota.mitsuki@gmail.com>
2026-07-14 20:35:35 +0900

    chore: sdk 37 + jdk 21

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 configure<ApplicationExtension> {
     namespace = "io.github.toyota32k.dialog.sample"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "io.github.toyota32k.dialog.sample"
         minSdk = 33
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
@@ -28,9 +28,13 @@ configure<ApplicationExtension> {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 dependencies {
