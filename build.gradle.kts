ea5f5fc8b36ad0de26b38dfbda4ce5a2e3d0433f
toyota-m2k <toyota.mitsuki@gmail.com>
2026-08-25 13:36:43 +0900

    chore: AGP 9.3.2 / update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
+//  kotlinのバージョンを指定する場合は kotlinAndroidプラグインを使うが、利用側と揃える必要があるので非推奨
+//    alias(libs.plugins.kotlinAndroid) apply false
     id("maven-publish")
 }
